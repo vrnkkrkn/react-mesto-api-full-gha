97a30360afa6ec1406a41b2d9103db37ddc73c2d
Veronika Karkan <veronikakarkan@gmail.com>
2023-10-15 23:01:14 +0300

    jwt fix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const { JWT_SECRET = 'mesto' } = process.env;
 const {
   BadRequestError,
   NotFoundError,
