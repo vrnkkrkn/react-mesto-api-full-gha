bbc186dcff4da1bfcfa56fbadb0a2d6182e10583
Veronika Karkan <veronikakarkan@gmail.com>
2023-10-19 19:32:16 +0300

    next() fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -21,5 +21,5 @@ module.exports = (req, res, next) => {
 
   req.user = payload; // записываем пейлоуд в объект запроса
 
-  next(); // пропускаем запрос дальше
+  return next(); // пропускаем запрос дальше
 };
